7f767313e42bc4ddd1d345749c1346a064a91823
dawsonbooth <github@dawsonbooth.com>
2021-03-15 17:41:05 -0500

    Fix configuration class

--- dustpan/config.py
+++ dustpan/config.py
@@ -1,34 +1,61 @@
 from __future__ import annotations
 
 import argparse
+import enum
 from pathlib import Path
 from typing import Set
 
-DEFAULT_CLEAN = {"**/*.pyc"}
+DEFAULT_PATTERNS = {"**/*.pyc"}
+
+
+class Verbosity(enum.Enum):
+    QUIET = enum.auto()
+    NORMAL = enum.auto()
+    VERBOSE = enum.auto()
 
 
 class Configuration:
     directories: Set[Path]
-    additional: Set[Path]
-    protect: Set[Path]
+    patterns: Set[Path]
+    verbosity: Verbosity
 
-    quiet: bool
-    verbose: bool
+    def __init__(
+        self,
+        directories: Set[Path],
+        additional: Set[Path] = set(),
+        protect: Set[Path] = set(),
+        quiet: bool = False,
+        verbose: bool = False,
+    ) -> None:
+        self.directories = set(map(lambda p: p.resolve(), directories))
+        self.patterns = (DEFAULT_PATTERNS | additional) - protect
+
+        self.verbosity = Verbosity.NORMAL
+        if quiet:
+            self.verbosity = Verbosity.QUIET
+        elif verbose:
+            self.verbosity = Verbosity.VERBOSE
 
     def __or__(self, o: Configuration) -> Configuration:
-        return self.__dict__.update(o.__dict__)
+        self.__dict__.update(o.__dict__)
+        return self
 
 
-def parse_pyproject(self) -> Configuration:
+def parse_pyproject() -> Configuration:
     # TODO
-    pass
+    return Configuration({Path.cwd()})
 
 
 def parse_arguments() -> Configuration:
+
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("directories", type=Path, nargs="+", help="Root directories to search")
-    parser.add_argument("-a", "--additional", type=Path, nargs="+", help="Additional path patterns for removal")
-    parser.add_argument("-p", "--protect", type=Path, nargs="+", help="Path patterns to protect from removal")
+    parser.add_argument("directories", type=Path, nargs="+", default=[Path.cwd()], help="Root directories to search")
+    parser.add_argument(
+        "-a", "--additional", type=str, nargs="+", default=[], help="Additional path patterns for removal"
+    )
+    parser.add_argument(
+        "-p", "--protect", type=str, nargs="+", default=[], help="Path patterns to protect from removal"
+    )
 
     verbosity = parser.add_mutually_exclusive_group()
     verbosity.add_argument("-q", "--quiet", type=bool, default=False, help="Be quiet")
@@ -36,16 +63,8 @@ def parse_arguments() -> Configuration:
 
     args = parser.parse_args()
 
-    config = Configuration()
-
-    config.directories = args.directories
-    config.additional = args.additional
-    config.protect = args.protect
-    config.quiet = args.quiet
-    config.verbose = args.verbose
+    return Configuration(set(args.directories), set(args.additional), set(args.protect), args.quiet, args.verbose)
 
 
 def get_configuration() -> Configuration:
-
-    config_toml = parse_pyproject()
-    config_cli = parse_arguments()
+    return parse_pyproject() | parse_arguments()
