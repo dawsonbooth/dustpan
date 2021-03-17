227e7a068161cfc6d0bbca901aa6d9785c046c3e
dawsonbooth <github@dawsonbooth.com>
2021-03-16 23:03:59 -0500

    Extract main functions

--- dustpan/__main__.py
+++ dustpan/__main__.py
@@ -1,56 +1,77 @@
+import os
+from pathlib import Path
 from sys import exit
+from typing import Iterable, Set
 
 from colorama import Style
 
-from . import DEFAULT_IGNORE, DEFAULT_PATTERNS, remove
+from . import DEFAULT_IGNORE, DEFAULT_PATTERNS, remove, search
 from .config import CONFIG
 
 
-def print_verbose(message: str, *args, **kwargs) -> None:
-    print(f"{Style.DIM}{message}{Style.RESET_ALL}", *args, **kwargs)
+def output(message: str, *args, verbose: bool = False, **kwargs):
+    if not verbose:
+        print(message, *args, **kwargs)
+    elif verbose and CONFIG.verbose:
+        print(f"{Style.DIM}{message}{Style.RESET_ALL}", *args, **kwargs)
+
+
+def paths_to_remove(patterns: Iterable[str], ignore: Iterable[str]) -> Set[Path]:
+    paths = set()
+
+    for path in search(*CONFIG.directories, patterns=patterns, ignore=[]):
+        paths.add(path)
+        output(f"Found: {path}", verbose=True)
+
+    for path in search(*CONFIG.directories, patterns=ignore, ignore=[]):
+        if path in paths:
+            paths.remove(path)
+        output(f"Ignored: {path}", verbose=True)
+
+    return paths
+
+
+def empty_directories() -> Set[Path]:
+    paths = set()
+    for path in search(*CONFIG.directories, patterns=["*"], ignore=DEFAULT_IGNORE | CONFIG.ignore):
+        if path.exists() and path.is_dir():
+            with os.scandir(path) as scan:
+                if next(scan, None) is None:
+                    paths.add(path)
+                    output(f"Found: {path}", verbose=True)
+    return paths
+
+
+def remove_paths(paths: Iterable[Path]) -> int:
+    paths = set(paths)
+    for path in paths:
+        remove(path)
+        output(f"Removing: {path}", verbose=True)
+    return len(paths)
 
 
 def main() -> int:
+    num_removed = 0
+
     paths = set()
 
-    for directory in CONFIG.directories:
-        for pattern in DEFAULT_PATTERNS:
-            for path in directory.rglob(pattern):
-                if path.exists():
-                    paths.add(path)
-                    if CONFIG.verbose:
-                        print_verbose(f"Found: {path}")
+    output("Default search...", verbose=True)
+    paths |= paths_to_remove(DEFAULT_PATTERNS, DEFAULT_IGNORE)
 
-        for pattern in DEFAULT_IGNORE:
-            for path in directory.rglob(pattern):
-                if path in paths:
-                    paths.remove(path)
-                if CONFIG.verbose:
-                    print_verbose(f"Ignored: {path}")
+    output("Custom search...", verbose=True)
+    paths |= paths_to_remove(CONFIG.patterns, CONFIG.ignore)
 
-        for pattern in CONFIG.patterns:
-            for path in directory.rglob(pattern):
-                if path.exists():
-                    paths.add(path)
-                    if CONFIG.verbose:
-                        print_verbose(f"Found: {path}")
+    output("Removing paths...", verbose=True)
+    num_removed += remove_paths(paths)
 
-        for pattern in CONFIG.ignore:
-            for path in directory.rglob(pattern):
-                if path in paths:
-                    paths.remove(path)
-                if CONFIG.verbose:
-                    print_verbose(f"Ignored: {path}")
+    if CONFIG.remove_empty_directories:
+        output("Empty directory search...", verbose=True)
+        empty = empty_directories()
 
-    for path in paths:
-        remove(path)
-        if CONFIG.verbose:
-            print_verbose(f"Removing: {path}")
+        output("Empty directory search...", verbose=True)
+        num_removed += remove_paths(empty)
 
-    # TODO: Optionally remove all blank directories
-
-    if not CONFIG.quiet:
-        print(f"{len(paths)} paths removed")
+    output(f"{num_removed} paths removed")
 
     return 0
 
